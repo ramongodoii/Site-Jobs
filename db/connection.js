513f622dd253b1f8952ab12e9a389cd8b8d5d81f
Ramon Godoi <ramon.dt4@gmail.com>
2024-11-04 19:38:39 -0300

    atualizando bando de dados 1.3

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,19 @@
+const Sequelize = require('sequelize');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
 const sequelize = isProduction ? new Sequelize('postgresql://postgres:QCFUbWfjPxmWteAEklWUAgpgRnZOLREV@postgres.railway.internal:5432/railway', {
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
-  }) : new Sequelize({
-    dialect: 'sqlite',
-    storage: './db/app.db'
-  });
+  }
+}) : new Sequelize({
+  dialect: 'sqlite',
+  storage: './db/app.db'
+});
+
+module.exports = sequelize;
